fa8a46d5be7aab3fea460bb9b2130033850f794c
Shahzod <yuldashev.1996@gmail.com>
2024-05-06 12:40:17 -0400

    Initilized mongo

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json())
+app.use(express.static('public'))
+
+
+
+const PORT = 3001;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        dbName: 'BecomeACoach',
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} did not connect`));
